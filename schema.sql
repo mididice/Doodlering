5f5f601f981d02234199e431d32a77954378f36b
nine-hundred <ninehundred@outlook.kr>
2020-12-02 22:51:35 +0900

    done

--- schema.sql
+++ schema.sql
@@ -41,7 +41,6 @@ CREATE TABLE IF NOT EXISTS `schema`.`Words` (
   `word` VARCHAR(255) NOT NULL,
   `Play_id` INT NOT NULL,
   `Play_Games_key` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Play_id`, `Play_Games_key`),
   CONSTRAINT `fk_Words_Play1`
     FOREIGN KEY (`Play_id` , `Play_Games_key`)
     REFERENCES `schema`.`Play` (`id` , `Games_key`)
@@ -57,7 +56,6 @@ CREATE TABLE IF NOT EXISTS `schema`.`Coordinate` (
   `coordinate` VARCHAR(255) NOT NULL,
   `Play_id` INT NOT NULL,
   `Play_Games_key` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Play_id`, `Play_Games_key`),
   CONSTRAINT `fk_Coordinate_Play1`
     FOREIGN KEY (`Play_id` , `Play_Games_key`)
     REFERENCES `schema`.`Play` (`id` , `Games_key`)
@@ -65,6 +63,24 @@ CREATE TABLE IF NOT EXISTS `schema`.`Coordinate` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `schema`.`Sentences` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `sentence` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+ALTER TABLE `schema`.`Play` 
+DROP COLUMN `sentence`;
+
+ALTER TABLE `schema`.`Coordinate` 
+CHANGE COLUMN `coordinate` `x` VARCHAR(255) NOT NULL AFTER `Play_Games_key`,
+ADD COLUMN `y` VARCHAR(255) NOT NULL AFTER `x`,
+ADD COLUMN `dx` VARCHAR(255) NOT NULL AFTER `y`,
+ADD COLUMN `dy` VARCHAR(255) NOT NULL AFTER `dx`;
+
+ALTER TABLE `schema`.`Words` 
+CHANGE COLUMN `word` `label` VARCHAR(255) NOT NULL ,
+ADD COLUMN `confidence` VARCHAR(255) NOT NULL AFTER `Play_Games_key`;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/foolin/goview/supports/ginview"
 	"github.com/gin-gonic/gin"
@@ -11,10 +12,14 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+var DB *sql.DB
+
 func main() {
-	DB, err := sql.Open("mysql", "root:pw@tcp(localhost:3306)/Doodlering")
+	var err error
+	DB, err = sql.Open("mysql", "root:1q2w3e4r5T!@@tcp(localhost:3306)/Doodlering")
 
 	if err != nil {
+		fmt.Println("fail to open db")
 		return
 	}
 	var key string
@@ -32,17 +37,19 @@ func main() {
 	r.GET("/playing/:key/:sequence", getPlayingks)
 	r.POST("/play/:key/:sequence", postPlayks)
 	r.GET("/story/:key/sequence", getStoryks)
+	r.GET("/story/:key/end", getStoryke)
 	r.GET("/ending/:key", getEndingk)
 	r.GET("/ending/:key/:sequence", getEndingks)
 	r.GET("/home", getHome)
 	r.GET("/howto", getHowto)
 	r.GET("/end/:key/:sequence", getEndks)
 	r.GET("/tale/:key/:sequence", taleks)
+	r.GET("/play/:key/:sequence", getPlayks)
 	r.Run(":8080")
 }
-
-func getEndks(c *gin.Context) {
-
+func getStoryke(c *gin.Context) {
+	c.Header("Content-Type", "text/html")
+	c.HTML(http.StatusOK, "story-end.html", gin.H{})
 }
 func getEndingks(c *gin.Context) {
 	c.Header("Content-Type", "text/html")
@@ -51,9 +58,6 @@ func getEndingks(c *gin.Context) {
 func getEndingk(c *gin.Context) {
 	c.Header("Content-Type", "text/html")
 	c.HTML(http.StatusOK, "playing-end.html", gin.H{})
-}
-func taleks(c *gin.Context) {
-
 }
 func getHome(c *gin.Context) {
 	c.Header("Content-Type", "text/html")
@@ -69,21 +73,165 @@ func getStart(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"key": uuid,
 	})
-	//insert DB
+	DB.Exec("INSERT INTO `Doodlering`.`Games` (`key`) VALUES ('" + uuid.String() + "');")
 }
 func getPlayingks(c *gin.Context) {
 	c.Header("Content-Type", "text/html")
 	c.HTML(http.StatusOK, "playing.html", gin.H{})
 }
-func postPlayks(c *gin.Context) {
 
+type Game struct {
+	Prediction []*Play    `json: "prediction"`
+	Coordinate [][]string `json: "coordinate"`
+}
+type Play struct {
+	Label      string `json : "label"`
+	Confidence string `json : "confidence"`
+}
+type End struct {
+	Coordinate [][]string `json: "coordinate"`
+	Answer     []*Play    `json: "answer"`
+}
+type Sentences struct {
+	Sentence string `json: "sentence"`
+}
+
+func postPlayks(c *gin.Context) {
+	key := c.Param("key")
+	sequence := c.Param("sequence")
+	var input Game
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(500, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+	DB.Exec("INSERT INTO `Doodlering`.`Play` (`Games_key`, `sequence`) " +
+		"VALUES ('" + key + "', '" + sequence + "');")
+	var id string
+	DB.QueryRow("SELECT id FROM Doodlering.Play " +
+		"where Games_key = '" + key + "'AND sequence = '" + sequence + "';").Scan(&id)
+	var query = ""
+	for i, _ := range input.Coordinate {
+		query = " INSERT INTO `Doodlering`.`Coordinate` (`Play_id`, `Play_Games_key`, `x`, `y`, `dx`, `dy`) VALUES ('" + id + "', '" + key + "', '" + input.Coordinate[i][0] + "', '" + input.Coordinate[i][1] +
+			"', '" + input.Coordinate[i][2] + "', '" + input.Coordinate[i][3] + "');"
+		DB.Exec(query)
+	}
+	for i, _ := range input.Prediction {
+		query = "INSERT INTO `Doodlering`.`Words` (`label`, `Play_id`, `Play_Games_key`, `confidence`)" +
+			"VALUES ('" + input.Prediction[i].Label + "', '" + id + "', '" + key + "', '" + input.Prediction[i].Confidence + "');"
+		DB.Exec(query)
+	}
+}
+
+func getPlayks(c *gin.Context) {
+	// key := c.Param("key")
+	sequence := c.Param("sequence")
+
+	var sentence string
+	DB.QueryRow("SELECT sentence FROM Doodlering.Sentences WHERE id =`" + sequence + "`;").Scan(&sentence)
+	c.JSON(200, Sentences{
+		Sentence: sentence,
+	})
+}
+
+type Tale struct {
+	Candidate  []string   `json:"candidate"`
+	Coordinate [][]string `json:"coordinate"`
+}
+
+func taleks(c *gin.Context) {
+	key := c.Param("key")
+	sequence := c.Param("sequence")
+	var id string
+	DB.QueryRow("SELECT id FROM Doodlering.Play " +
+		"where Games_key = '" + key + "'AND sequence = '" + sequence + "';").Scan(&id)
+
+	query := "SELECT label, confidence FROM Doodlering.Words " +
+		"where Play_id = " + id + " AND Play_Games_key = '" + key + "';"
+	rows, err := DB.Query(query)
+	if err != nil {
+		return
+	}
+	var words []Play
+	var label, confidence string
+	for rows.Next() {
+		rows.Scan(&label, &confidence)
+		words = append(words, Play{Label: label, Confidence: confidence})
+	}
+	sort.Slice(words, func(i, j int) bool {
+		return words[i].Confidence > words[j].Confidence
+	})
+	for _, tmp := range words {
+		fmt.Println(tmp.Confidence)
+	}
+	var tale Tale
+	var tmp []string
+	for i := 0; i < 2; i++ {
+		tmp = append(tmp, words[i].Label)
+	}
+	tale.Candidate = tmp
+
+	query = "SELECT x, y, dx, dy FROM Doodlering.Coordinate " +
+		"WHERE Play_Games_key = '" + key + "' AND Play_id = " + id + ";"
+	rows, err = DB.Query(query)
+	var x, y, dx, dy string
+	var cordiOutput [][]string
+	for rows.Next() {
+		rows.Scan(&x, &y, &dx, &dy)
+		var tmp []string
+		tmp = append(tmp, x, y, dx, dy)
+		cordiOutput = append(cordiOutput, tmp)
+	}
+	tale.Coordinate = cordiOutput
+	c.JSON(200, tale)
+}
+func getEndks(c *gin.Context) {
+	key := c.Param("key")
+	sequence := c.Param("sequence")
+	var id string
+	DB.QueryRow("SELECT id FROM Doodlering.Play " +
+		"where Games_key = '" + key + "'AND sequence = '" + sequence + "';").Scan(&id)
+	var output End
+	fmt.Println("id:" + id)
+	var query string
+	query = "SELECT label, confidence FROM Doodlering.Words " +
+		"where Play_id = " + id + " AND Play_Games_key = '" + key + "';"
+	rows, err := DB.Query(query)
+	if err != nil {
+		fmt.Println("fail to fetch")
+		return
+	}
+	var label, confidence string
+	var answers []*Play
+	for rows.Next() {
+		rows.Scan(&label, &confidence)
+		row := Play{
+			Confidence: confidence,
+			Label:      label,
+		}
+		answers = append(answers, &row)
+	}
+	output.Answer = answers
+	query = "SELECT x, y, dx, dy FROM Doodlering.Coordinate " +
+		"WHERE Play_Games_key = '" + key + "' AND Play_id = " + id + ";"
+	rows, err = DB.Query(query)
+	var x, y, dx, dy string
+	var cordiOutput [][]string
+	for rows.Next() {
+		rows.Scan(&x, &y, &dx, &dy)
+		var tmp []string
+		tmp = append(tmp, x, y, dx, dy)
+		cordiOutput = append(cordiOutput, tmp)
+	}
+	output.Coordinate = cordiOutput
+	c.JSON(200, output)
 }
 func getStoryks(c *gin.Context) {
 	c.Header("Content-Type", "text/html")
 	c.HTML(http.StatusOK, "story.html", gin.H{})
 }
 func getEndk(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "end",
-	})
+	c.Header("Content-Type", "text/html")
+	c.HTML(http.StatusOK, "playing-end.html", gin.H{})
 }
