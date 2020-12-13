dcee46f133e8c3ed020d270330dc7a117985ace4
nine-hundred <ninehundred@outlook.kr>
2020-12-13 17:23:40 +0900

    remvoe port

--- server.go
+++ server.go
@@ -45,13 +45,13 @@ func main() {
 	r.GET("/play/:key/:sequence", getPlayks)
 	r.GET("/", redirectHome)
 
-	r.Run()
-	// server := &http.Server{
-	// 	Addr:    "",
-	// 	Handler: r,
-	// }
-	// server.SetKeepAlivesEnabled(false)
-	// server.ListenAndServe()
+	//r.Run()
+	 server := &http.Server{
+	 	Addr:    "",
+	 	Handler: r,
+	 }
+	server.SetKeepAlivesEnabled(false)
+	server.ListenAndServe()
 }
 func redirectHome(c *gin.Context) {
 	c.Redirect(http.StatusMovedPermanently, "/home")
